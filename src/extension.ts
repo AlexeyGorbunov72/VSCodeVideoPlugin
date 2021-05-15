733f574049a5cc7a235b6f61d9d7f82456ea5cf6
Alexey Gorbunov <alexeygorbunov72@gmail.com>
2021-05-15 15:39:12 +0300

    fix recursive stack overflow

--- src/extension.ts
+++ src/extension.ts
@@ -5,6 +5,7 @@ export function activate(context: vscode.ExtensionContext) {
 	let disposable = vscode.commands.registerCommand('playvideovscode.helloWorld',   () => {});
 	let playVideo = vscode.commands.registerCommand('playvideovscode.playVideo', () => {
 		vscode.window.showInformationMessage("Start video!");
+
 		const file = readFileSync('/Users/Retard/video/config', 'utf-8');
 		var frameCount: number = +file;
 		const { activeTextEditor  } = vscode.window;
@@ -22,11 +23,11 @@ async function changeFrame(i: number, framesAmount: number, document: vscode.Tex
 			return;
 		}
 		let text = readFileSync('/Users/Retard/video/' + i + '.txt', 'utf-8') + i;
-		const edit = new vscode.WorkspaceEdit();
+		let edit = new vscode.WorkspaceEdit();
 		edit.replace(document.uri, new vscode.Range(document.lineAt(0).range.start, document.lineAt(document.lineCount - 1).range.end), text);
-		await vscode.workspace.applyEdit(edit);
-		changeFrame(i + 1, framesAmount, document);
-
+		vscode.workspace.applyEdit(edit).then(value => {
+			changeFrame(i + 1, framesAmount, document);
+		});
 }
 // this method is called when your extension is deactivated
 export function deactivate() {}
